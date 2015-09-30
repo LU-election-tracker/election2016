400f7b7f32e16c1272b7c79a0d622c90f1545dbe
HenryWard <henry_ward@yahoo.com>
2015-09-29 21:24:16 -0500

    Archiving and Formatting
    Archives RCP polling files on a daily basis. Polls with sample
    information are parsed into two columns for sample size and type.

--- get_polls.R
+++ get_polls.R
@@ -2,13 +2,26 @@ library(rvest)
 library(tidyr)
 
 # Scrapes a given Real Clear Politics page and writes updated csv files with
-# most recent data and complete poll data
-scrape_rcp <- function(rcp_address, recent_out, full_out) {
+# most recent data and complete poll data. Splits sample column into number
+# and type if sample available (not available for GOP candidates)
+scrape_rcp <- function(rcp_address, recent_out, full_out, sample = TRUE) {
+  
+  # Reads recent poll table as a data frame
   rcp <- html(rcp_address)
   recent <- rcp %>% html_nodes(".data") %>% .[[1]] %>% html_table()
   recent <- separate(data = recent, col = Date, into = c("Start", "End"), sep = " - ")
+  if (sample) {
+    recent <- separate(data = recent, col = Sample, into = c("Sample", "Type"), sep = "\\ ", fill = "left")
+  }
+  
+  # Reads full poll table as a data frame
   full <- rcp %>% html_nodes(".data") %>% .[[2]] %>% html_table()
   full <- separate(data = full, col = Date, into = c("Start", "End"), sep = " - ")
+  if (sample) {
+    full <- separate(data = full, col = Sample, into = c("Sample", "Type"), sep = "\\ ", fill = "left")
+  }
+  
+  # Writes both tables out as csvs, separated by tabs
   write.table(recent, recent_out, sep = "\t", quote = FALSE, row.names = FALSE)
   write.table(full, full_out, sep = "\t", quote = FALSE, row.names = FALSE)
 }
@@ -22,7 +35,8 @@ update_rcp <- function(folder) {
              file.path(folder, "rcp_dem_full.csv"))
   scrape_rcp(reps_address, 
              file.path(folder, "rcp_gop_recent.csv"),
-             file.path(folder, "rcp_gop_full.csv"))
+             file.path(folder, "rcp_gop_full.csv"),
+             sample = FALSE)
 }
 
 # Creates plots for a given rcp table
@@ -30,12 +44,45 @@ plot_rcp <- function(table_file) {
   table - read.csv(file, sep = "\t")
 }
 
+# Archives old poll information
+archive <- function(main_folder, data_folder) {
+  
+  # Gets all current files
+  dem_recent <- "rcp_dem_recent.csv"
+  dem_full <- "rcp_dem_full.csv"
+  gop_recent <- "rcp_gop_recent.csv"
+  gop_full <- "rcp_gop_full.csv"
+  
+  # Creates an archive folder for the current day. Overwrites if exists
+  archive <- file.path(data_folder, as.character(Sys.Date()))
+  dir.create(archive, showWarnings = FALSE)
+  
+  # Archives current files. Overwrites if exists
+  file.copy(file.path(main_folder, dem_recent), file.path(archive, dem_recent),
+            overwrite = TRUE)
+  file.copy(file.path(main_folder, dem_full), file.path(archive, dem_full), 
+            overwrite = TRUE)
+  file.copy(file.path(main_folder, gop_recent), file.path(archive, gop_recent), 
+            overwrite = TRUE)
+  file.copy(file.path(main_folder, gop_full), file.path(archive, gop_full), 
+            overwrite = TRUE)
+}
+
+
 # Main update function. Updates polling data and graphs.
 track <- function() {
-  folder <- file.path("C:", "Users", "Navi", "Dropbox", "Public", "Junior Year", 
+  main_folder <- file.path("C:", "Users", "Navi", "Dropbox", "Public", "Junior Year", 
                       "R", "election2016", "Poll_data")
-  if(dir.exists(folder) == FALSE) {
-    dir.create(folder)
-  }
-  update_rcp(folder)
+  data_folder <- file.path(main_folder, "Archive")
+  if(dir.exists(main_folder) == FALSE) { dir.create(main_folder) }
+  if(dir.exists(data_folder) == FALSE) { dir.create(data_folder) }
+  update_rcp(main_folder)
+  invisible(archive(main_folder, data_folder))
 }
+
+# Sample debug code
+# source("C:\\Users\\Navi\\Dropbox\\Public\\Junior Year\\R\\election2016\\get_polls.R")
+# track()
+# dem <- read.csv(file.path(main_folder, "rcp_dem_full.csv"), sep = "\t")
+# gop <- read.csv(file.path(main_folder, "rcp_gop_full.csv"), sep = "\t")
+# file.info(file.path(main_folder, "rcp_dem_full.csv"))$mtime
