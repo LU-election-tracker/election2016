3d7c45d9702ae77d5bd2534e1c45d236b9cef641
HenryWard <henry_ward@yahoo.com>
2015-10-11 13:58:49 -0500

    Updated About Page
    Replaced previous Shiny theme with a more striking one, and updated the
    about page.

--- ui.R
+++ ui.R
@@ -13,11 +13,17 @@ funding_types <- c("All" = "all", "Campaign" = "campaign", "Super PAC" = "super_
                    "Other" = "other")
 funding_groups <- c("All" = "all", "Democrats" = "dem", "Republicans" = "gop")
 
+rcp_dem_address <- "http://www.realclearpolitics.com/epolls/2016/president/us/2016_democratic_presidential_nomination-3824.html"
+rcp_gop_address <- "http://www.realclearpolitics.com/epolls/2016/president/us/2016_republican_presidential_nomination-3823.html"
+os_address <- "https://www.opensecrets.org/pres16/outsidegroups.php"
+git_address <- "https://github.com/LU-election-tracker/election2016"
+lawrence_address <- "http://www.lawrence.edu/"
+
 ######
 ### SITE UI
 ######
 
-shinyUI(navbarPage("LU Election Tracker 2016", theme = shinytheme("flatly"),
+shinyUI(navbarPage("LU Election Tracker 2016", theme = shinytheme("cerulean"),
     
   tabPanel("Democrats",
            sidebarLayout(
@@ -76,10 +82,28 @@ shinyUI(navbarPage("LU Election Tracker 2016", theme = shinytheme("flatly"),
   ),
 
   tabPanel("About",
-           p("Visit us on ",
-           a("Github.", 
-             href = "https://github.com/LU-election-tracker/election2016"),
-           "All code licensed under the ",
-           a("MIT license.", href = "https://www.tldrlegal.com/l/mit")
-           ))
+           sidebarLayout(
+             sidebarPanel(
+               p(a("Democrat Poll Info", href = rcp_dem_address)),
+               p(a("Republican Poll Info", href = rcp_gop_address)),
+               p(a("Github", href = git_address))
+             ),
+             mainPanel(
+               p(h3("Data")),
+               
+               p("All poll data is scraped from Real Clear Politics, and all funding
+                 data is scraped from Open Secrets. Links to the data sources are
+                 on the left. Poll numbers are averaged by week per candidate before
+                 plotting. Data are updated daily at midnight CST." ),
+               
+               p(h3("Us")),
+               
+               p("Created by Lawrence University student Henry Ward and Prof. Adam 
+                 Loy. Source code for the entirety of this site is available on ",
+                 a("Github.", href = "https://github.com/LU-election-tracker/election2016"),
+                 "All code is licensed under the ",
+                 a("MIT license.", href = "https://www.tldrlegal.com/l/mit")
+               ))
+             )
+           )
 ))
